62decccf5659dcbedf1f7824b3e8e3a09bd3090a
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-19 10:46:27 +0530

    Disable basic auth.

--- src/main/java/com/gsr/libraryauth/configurations/WebSecurityConfiguration.java
+++ src/main/java/com/gsr/libraryauth/configurations/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -33,5 +34,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter  {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().disable();
+        http.httpBasic().disable();
     }
 }
