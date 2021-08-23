be27e89d0755838a2c5a795dbacd725eca079b63
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-23 11:43:21 +0530

    Enabling client_credentials flow.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity, refresh_token_validity, resource_ids, authorized_grant_types, additional_information)
--- VALUES ('mobile', '{bcrypt}$2a$10$gPhlXZfms0EpNHX0.HHptOhoFD1AoxSr/yUIdTqA8vtjeP4zi0DDu', 'http://localhost:8080/code', 'READ,WRITE', '3600', '10000', 'book,user', 'authorization_code,password,refresh_token,implicit', '{}');
+-- VALUES ('mobile', '{bcrypt}$2a$10$gPhlXZfms0EpNHX0.HHptOhoFD1AoxSr/yUIdTqA8vtjeP4zi0DDu', 'http://localhost:8080/code', 'READ,WRITE', '3600', '10000', 'book,user', 'authorization_code,password,refresh_token,implicit,client_credentials', '{}');
 --
 -- INSERT INTO PERMISSION (id, NAME) VALUES
 -- (1, 'borrow_book'),
